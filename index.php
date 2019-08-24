54a550f000ef635c717b4f4da513052ffea172d9
SusmitKudari <kudarisusmit07@gmail.com>
2019-08-24 11:29:56 +0530

    changed the index

--- index.php
+++ index.php
@@ -1,76 +1,78 @@
+<!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Responsive Navigation Bar</title>
-	<link rel="stylesheet" href="style.css">
-	<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/font-awesome.min.js">
+	<title>Fashion</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!--link-->
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-	<h1>dress and cosmetics and hairbands</h1>
+	<div class="container">
+      <div class="nav">
+         <img class="nav__logo" src="./img/logo.jpg">
+            <div class="nav__bar">
+              <i class="fa fa-bars bars"></i>
+            </div>
+            <div class=nav__links>
+            	<a href="#">Home</a>
+                <a href="#"> About</a>
+                <a href="#">Blog</a>
+            	<a href="#">Contact</a>
+            	<p>|</p>
+            	<a href="#">sign-up</a>
+            	<a href="#">Log In</a>
+            </div>
+       </div>
 
-		<div class="wrapper">
-			<header>
-				<nav>
-					<div class="menu-icon">
-						<i class="fa fa-bars fa-2x"></i>
-						
-					</div>
-						<div class="logo">
-							LOGO
-						</div>
+       <div class="data">
 
-						<div class="menu">
-							<ul>
-								<li><a href="#">Home</a></li>
-								<li><a href="#">About</a></li>
-								<li><a href="#">Blog</a></li>
-								<li><a href="#">Contact </a></li> 
-							</ul>
-						</div>
-				</nav>
-				
-			</header>
-				<div class="content">
-					<p>
-						Create Secure Websites That Work Across Devices Without Programming. Sign up Now. Automatic Backups. Archive Emails & Chats. Google Email Hosting. Draft Emails Offline. Easily Import Old Emails. Custom Business Email. Auto Suggest for Replies. Phishing Protection. Save Time w/ Smart Reply. Up To 30 Email Aliases.
-					</p>
+       	    <h1>Fashion For Everyone.</h1>
+       	   <p>Millions of Variety. No credit card needed.</p>
+       	     <a  class="data__link" href="#">OFFERS</a>
+         
+       </div>
+   </div>
+   <!--<footer class="foot_container">
+      
+        <div class="ft_containers">
 
-					<p>
-						Create Secure Websites That Work Across Devices Without Programming. Sign up Now. Automatic Backups. Archive Emails & Chats. Google Email Hosting. Draft Emails Offline. Easily Import Old Emails. Custom Business Email. Auto Suggest for Replies. Phishing Protection. Save Time w/ Smart Reply. Up To 30 Email Aliases.
-					</p>
+          <div class="row">
+          <div class="foot__logo">
+            <img  class="ft__logo" src="spotify_logo (1).png" >
+          </div>
+        
+        <div class="foot__list1">
+          <h3 class="ft_text">Company</h3>
+          <ul>
+            <li><a>About</a></li>
+            <li><a>Jobs</a></li>
+            <li><a>For the Records</a></li>
+          </ul>
+          
+          
+          
+        </div>
+        <div class="foot__list1">
+          <h3 class="ft_text">Communities</h3>
+          <ul>
+            <li><a>For Artists</a></li>
+            <li><a>Developer</a></li>
+            <li><a>Brands</a></li>
+            <li><a>Investor</a></li>
+            <li><a>Vendors</a></li>
+          </ul>
+        </div>
 
 
-					<p>
-						Create Secure Websites That Work Across Devices Without Programming. Sign up Now. Automatic Backups. Archive Emails & Chats. Google Email Hosting. Draft Emails Offline. Easily Import Old Emails. Custom Business Email. Auto Suggest for Replies. Phishing Protection. Save Time w/ Smart Reply. Up To 30 Email Aliases.
-					</p>
-					
-				</div>
-
-		</div>
-
-			<script type="text/javascript">
-				
-				$(document).ready(function(){
-					$("menu-icon").on("click",function(){
-						$("nav-ul").toogleClass("showing");
-					});
-				});	
-
-				$("window").on("scroll",function(){
-					if($(window).scrollTop()){
-						$('nav').addClass('black');
-
-					}
-
-					else
-					{
-						$('nav').removeClass('black');
-					}
-				}) 
-			</script>
+        <div class="foot__list1">
+          <h3 class="ft_text">Useful links</h3>
+          <ul>
+            <li><a>Help</a></li>
+            <li><a>Free Mobile App</a></li>
+          </ul>
+          
+        </div>-->
 
 </body>
-</html>	
+</html>
